53acd7651e7692e70efb2cef2e2579be114ddc66
Rotella <rotella@wifi-eduroam-84-110.paris.inria.fr>
2026-03-04 15:21:00 +0100

    update TP TD

--- _docs/td6_exo1.py
+++ _docs/td6_exo1.py
@@ -3,23 +3,34 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
  
 keyhex = (os.urandom(4)).hex()
 key2 = keyhex+keyhex+keyhex+keyhex
+
+#permet de transformer une chaine en hexa en tableaux d'octets
 key = bytes.fromhex(key2)
+
 nonce = bytes.fromhex("664fe0e9ac9277efa4d8e2c5870a836a")
 print('Nonce =')
+
+#permet d'avoir un meilleur affichage qu'un tableau d'octets
 print(nonce.hex())
+
+#declaration du block cipher et du mode utilisé
 cipher = Cipher(algorithms.AES(key), modes.CTR(nonce))
+
+#objet à appeler
 encryptor = cipher.encryptor()
 ct = encryptor.update(b"Bonjour Monsieur, pouvez-vous ameliorer ma note ?") + encryptor.finalize()
 decryptor = cipher.decryptor()
 pt = decryptor.update(ct) + decryptor.finalize()
 
+#Il faudra peut etre commenter ceci. Cela vous est donne pour vous aider
 nonceReponse = os.urandom(16)
 cipher2 = Cipher(algorithms.AES(key), modes.CTR(nonceReponse))
 encryptor2 = cipher2.encryptor()
-ct2 = encryptor2.update(b"MESSAGEATROUVER") + encryptor2.finalize()
+ct2 = encryptor2.update(b"MESSAGE A TROUVER") + encryptor2.finalize()
 decryptor2 = cipher2.decryptor()
 pt2 = decryptor2.update(ct2) + decryptor2.finalize()
 
+print("Nonce")
 print("Chiffré envoyé")
 print(ct.hex())
 print("Nonce Réponse")
@@ -27,16 +38,24 @@ print(nonceReponse.hex())
 print("Chiffré répondu")
 print(ct2.hex())
 
-#MESSAGES REELLEMENT TRANSMIS SUR LE
+#les chiffres du prof et de l'etudiant
+ct_prof = bytes.fromhex("4e63aacd834529d18f507c302c2fedb8d28e11d642b559292f4af9401b7464207351456227becd82f3d914ccbfbaa6427429329615ce86953a87eb7881e245d6fc5a16a8775531a3125156ca7a7bffc26448eeb138931047b676ce37bc879aae2f45bb841e2b9cd6513ce3be80f146d94489b48f5bf1218924227e11e20995da0adb3d71fb37a5ea1f70")
+ct_etu = bytes.fromhex("9799bc41d104f0cb2255ea76791efe0fee40e08460d082577e4a78becf71e7c6c866e663ed8dd16ab007bcc150b96b2113")
+
+#Une fonction utile ? si i est un entier, i.to_bytes(4, 'big') permet de trsnformer un entier en octets
+n = 23974930
+v = n.to_bytes(4,'big')
+print(v)
+print(v.hex())
+
+#MESSAGES REELLEMENT TRANSMIS SUR LE CANAL
 #Nonce =
 #664fe0e9ac9277efa4d8e2c5870a836a
+#Nonce
 #Chiffré envoyé
-#92fb9874844441baeccb27ec38621ddea3c8c715530097c7e6e841f3117ed1e2b78a6796d75356924e23d819a1dd41fef8
+#9799bc41d104f0cb2255ea76791efe0fee40e08460d082577e4a78becf71e7c6c866e663ed8dd16ab007bcc150b96b2113
 #Nonce Réponse
-#7102906ef729a019f2f148d343ea7a35
+#efa35dd810a74f9fb769239de9933f19
 #Chiffré répondu
-#b1353e170c5f8fc018ba98bafaf8061baf2415619a8b7df1fcc14d57c9c727344d428e692320c60dfefaf5427abd8179d4641c3bca91d49b
-
-
-
+#4e63aacd834529d18f507c302c2fedb8d28e11d642b559292f4af9401b7464207351456227becd82f3d914ccbfbaa6427429329615ce86953a87eb7881e245d6fc5a16a8775531a3125156ca7a7bffc26448eeb138931047b676ce37bc879aae2f45bb841e2b9cd6513ce3be80f146d94489b48f5bf1218924227e11e20995da0adb3d71fb37a5ea1f70
 
